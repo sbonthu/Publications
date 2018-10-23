47da9309e03868eb23dff284acde9989b46947f3
kasiriveni <kasiriveni@gmail.com>
2018-10-23 14:56:45 +0530

    folder updated

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-         <h1>Login</h1>
+          <h1>Login</h1>
         </header>
       </div>
     );
